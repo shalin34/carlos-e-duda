a15a17e3a730f4b45879ea9ea0220cfb72181349
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-08-12 18:49:05 +0000

    atualiza

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ botao1.addEventListener("click", function(){
     
 })
 
+botao2.addEventListener("click", function(){
+    console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
+    caixaResultado2.classList.add("mostrar-caixa-resultado");
+    
+})
+
 
 
 
