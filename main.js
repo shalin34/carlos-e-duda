c172a6259b69fe7828c02838b927087d42839316
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-08-12 18:39:02 +0000

    Atualização

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".resultado-1");
-const caixaResultado2 = document.querySelector(".resultado-2");
 const textoResultado = document.querySelector(".textoResultado");
 const botao1 = document.querySelector(".caixa-alternativas .botao_1");
-const botao2 = document.querySelector(".caixa-alternativas .botao_2");
+
 console.log(botao1);
 
 /*Interceptar o click do botão*/
@@ -16,10 +15,6 @@ botao1.addEventListener("click", function(){
     
 })
 
-botao2.addEventListener("click", function(){
-    console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
-    caixaResultado2.classList.add("mostrar-caixa-resultado");
-    
-})
+
 
 
