e6d49d6e1d94a8577aa2f33df523d0773d3a04b3
shalin34 <110827688+shalin34@users.noreply.github.com>
2024-08-16 12:30:52 +0000

    vvv

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".resultado-1");
+const caixaResultado2 = document.querySelector(".resultado-2");
 const textoResultado = document.querySelector(".textoResultado");
 const botao1 = document.querySelector(".caixa-alternativas .botao_1");
+const botao2 = document.querySelector(".caixa-alternativas .botao_2");
 
 
 console.log(botao1);
@@ -13,8 +15,13 @@ console.log(botao1);
 botao1.addEventListener("click", function(){
     console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
     caixaResultado.classList.add("mostrar-caixa-resultado");
-    
 })
+
+botao2.addEventListener("click", function(){
+    console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
+    caixaResultado2.classList.add("mostrar-caixa-resultado");
+})     
+
 
 
 
